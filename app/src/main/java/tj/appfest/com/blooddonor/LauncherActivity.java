7bd6c6d5714be947ea24c3f102e7cbca9cab9334
Nitin <nitin.mesta@cleartax.in>
2017-06-28 11:21:16 +0530

    Initial commit

---
+++ app/src/main/java/tj/appfest/com/blooddonor/LauncherActivity.java
@@ -0,0 +1,13 @@
+package tj.appfest.com.blooddonor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luncher);
+    }
+}
