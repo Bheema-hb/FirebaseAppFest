2e94019a9d3792f17e779545e6b5f542838783d0
nitinmesta <nitin.mesta@cleartax.in>
2017-06-28 12:34:11 +0530

    Added the signin key

--- app/src/main/java/tj/appfest/com/blooddonor/LauncherActivity.java
+++ app/src/main/java/tj/appfest/com/blooddonor/LauncherActivity.java
@@ -1,8 +1,7 @@
 package tj.appfest.com.blooddonor;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class LauncherActivity extends AppCompatActivity {
 
